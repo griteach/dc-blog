c677f8e4502260864af4ee07fd4794801dfd0ac2
griteach <griteach22@gmail.com>
2023-08-14 12:44:22 +0900

    mememe

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <p className="bg-slate-900 text-center text-sm text-white py-2">
+      {"Don't forget to code your dream | All right Reserved"}
+    </p>
+  );
+}
