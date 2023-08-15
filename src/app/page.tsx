cbcebda9d70f03d3152c4cfc1a344b2dcefd064e
griteach <griteach22@gmail.com>
2023-08-15 22:46:30 +0900

    featured posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import FeaturedPosts from "./components/FeaturedPosts";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedPosts />
     </>
   );
 }
