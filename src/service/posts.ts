60684e4caebaad11bd3254ef48a226dd5b3c83b0
griteach <griteach22@gmail.com>
2023-08-16 23:52:36 +0900

    call posts

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -3,12 +3,16 @@ import path from "path";
 
 export type Post = {
   title: string;
-  desctiption: string;
+  description: string;
   date: Date;
   category: string;
   path: string;
   featured: boolean;
 };
+
+export async function getFeaturedPosts(): Promise<Post[]> {
+  return getAllPosts().then((posts) => posts.filter((post) => post.featured));
+}
 
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), "data", "posts.json");
